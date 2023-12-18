682472e46e933f7d4c903c0d0b0191f43f5823ce
Huip van den Ende <hey@huip.me>
2023-12-18 16:34:11 +0100

    Add Likert5.vue

---
+++ components/Likert5.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="text-center">
+    <div>{{ question }}</div>
+    <v-rating
+      v-model="rating"
+      background-color="indigo lighten-3"
+      clearable
+      color="red"
+      large
+    ></v-rating>
+    <span class="font-weight-bold">
+        {{ rating }}
+      </span>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    question: String,
+  },
+  data() {
+    return {
+      rating: 0,
+    };
+  },
+});
+</script>
